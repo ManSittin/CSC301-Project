e6c6520533490d1fd02392b6608f87afa17c7445
Sani Suljanovic <sani.suljanovic@mail.utoronto.ca>
2024-02-04 22:05:30 -0500

    [1.5] added test users to database.sql

--- server.php
+++ server.php
@@ -1,24 +1,30 @@
 <?php
 
 class Controller {
-    
+    public function nameTBD() {
+        $request_method = $_SERVER['REQUEST_METHOD'];
+        switch ($request_method) {
+            case 'POST':
+                $this->handlePost();
+                break;
+            default:
+                http_response_code(405); // TODO use a good response
+                echo "Request method not allowed";
+                break;
+        }
+    }
+
+    private function handlePost() {
+        $username = $_POST['username'];
+        $email = $_POST['email'];
+        $
+    }
 }
 
-$request_method = $_SERVER['REQUEST_METHOD'];
 
 
-switch ($request_method) {
-    case 'GET':
-        handleGet();
-        break;
-    case 'POST':
-        handlePost();
-        break;
-    default:
-        http_response_code(405); // TODO use a good response
-        echo "Request method not allowed";
-        break;
-}
+
+
 
 
 function handleGet() {
@@ -36,7 +42,7 @@ function handlePost() {
     /*
 
     $param = $_POST['']
+*/
 }
-
 
 ?>
