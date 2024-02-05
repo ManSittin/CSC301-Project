35885331375511ce272c7984a1ab507a32acc3e0
Sani Suljanovic <sani.suljanovic@mail.utoronto.ca>
2024-02-04 22:08:36 -0500

    [1.5] fixed typos in database.sql

--- database.sql
+++ database.sql
@@ -9,16 +9,13 @@ CREATE TABLE IF NOT EXISTS Users (
 );
 
 -- FOR TESTING remove in prod
-INSERT INTO Users (username, email, first_name, last_name, password) {
-    VALUES
-    ("userAA", "AA@email.com", "A", "A", "password"),
-    ("userBB", "BB@email.com", "B", "B", "password"),
-    ("userCC", "CC@email.com", "C", "C", "password"),
-    ("userDD", "DD@email.com", "D", "D", "password"),
-    ("userEE", "EE@email.com", "E", "E", "password")
-}
-
-)
+INSERT INTO Users (username, email, first_name, last_name, password)
+VALUES
+("userAA", "AA@email.com", "A", "A", "password"),
+("userBB", "BB@email.com", "B", "B", "password"),
+("userCC", "CC@email.com", "C", "C", "password"),
+("userDD", "DD@email.com", "D", "D", "password"),
+("userEE", "EE@email.com", "E", "E", "password");
 
 -- LATER can add config files for themes and such
 -- password should be hashed
