86c4578b81ccc575fbc98771eb9dcdb5a5204b18
santonip <sebastian.santoni@mail.utoronto.ca>
2024-02-29 20:00:13 -0500

    [Version 2.0] fixed bug where recent notes heading appeared in the wrong place

--- notes-all.php
+++ notes-all.php
@@ -112,7 +112,6 @@ $numNotes = 0;
         <!-- hard-coded note for now; will pull this in when a note is selected -->
                 
         <div id="note info">
-        <h2>Recent Notes</h2>
         <?php
             if ($numNotes > 0) {
                 while ($note = mysqli_fetch_assoc($notes)) {
